03e42fc633a22d2038ab30449de550e37f46c036
tanayBR <tanay.a@bamboorose.com>
2024-11-07 13:40:41 +0530

    web worker

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const worker = new Worker(`
     const minutes = now.getMinutes();
     
     // Check if it's 10:00 AM
-    if (hours === 13 && minutes === 40) {
+    if (hours === 13 && minutes === 45) {
       parentPort.postMessage('SEND_RATES');
     }
     
